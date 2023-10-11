6e18aa98774fbc85085bffd06d79b98235ba784e
Bill Mills <bill.mills@linaro.org>
2023-10-11 17:31:42 -0400

    Sphinx: Make the style align with new Doxygen style
    * Use same Logo and background
    * Make it look similar when narrow (fix menu color)
    * Make version more visible.
    * Give some red accents in the nav bar
    * Make the RTD version header red
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- conf.py
+++ conf.py
@@ -91,6 +91,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_style = 'css/mystyle.css'
+html_logo  = '_static/images/openAMP_combox_dark_trim.svg'
+
+html_theme_options = {
+    'logo_only': True
+}
 
 # below copied / hacked from Zephyr projects' zephyr/doc/conf.py
 docs_title = "Docs / {}".format(version if is_release else "Latest")
